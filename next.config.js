602967358fd5d9378de1b4df29b8fc2e62a0f252
Luka <47296785+lukadev-0@users.noreply.github.com>
2022-12-28 15:33:11 +0100

    Initial Commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+  defaultShowCopyCode: true,
+});
+
+module.exports = withNextra(nextConfig);
